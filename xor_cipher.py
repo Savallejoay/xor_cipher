ad44fbba0c9835b2c5ffba9eb31d7d06e06f0c84
Savallejoay <savallejoay@uide.edu.ec>
2026-05-07 14:39:12 -0500

    Add files via upload

---
+++ xor_cipher.py
@@ -0,0 +1,27 @@
+import random
+
+texto = "UIDE2026"
+binario = ""
+
+for letra in texto:
+    binario += format(ord(letra), "08b")
+
+llave = ""
+for i in range(len(binario)):
+    llave += str(random.randint(0,1))
+
+cifrado = ""
+for i in range(len(binario)):
+    cifrado += str(int(binario[i]) ^ int(llave[i]))
+
+descifrado = ""
+for i in range(len(cifrado)):
+    descifrado += str(int(cifrado[i]) ^ int(llave[i]))
+
+resultado = ""
+for i in range(0, len(descifrado), 8):
+    resultado += chr(int(descifrado[i:i+8], 2))
+
+print("Llave:", llave)
+print("Cifrado:", cifrado)
+print("Descifrado:", resultado)
