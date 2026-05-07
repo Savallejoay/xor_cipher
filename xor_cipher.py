71db312bd6c731b6741f04fe8110c674a75ea3ba
Savallejoay <savallejoay@uide.edu.ec>
2026-05-07 17:56:46 -0500

    Update xor_cipher.py

--- xor_cipher.py
+++ xor_cipher.py
@@ -14,6 +14,10 @@ cifrado = ""
 for i in range(len(binario)):
     cifrado += str(int(binario[i]) ^ int(llave[i]))
 
+texto_cifrado = ""
+for i in range(0, len(cifrado), 8):
+    texto_cifrado += chr(int(cifrado[i:i+8], 2))
+
 descifrado = ""
 for i in range(len(cifrado)):
     descifrado += str(int(cifrado[i]) ^ int(llave[i]))
@@ -21,8 +25,8 @@ for i in range(len(cifrado)):
 resultado = ""
 for i in range(0, len(descifrado), 8):
     resultado += chr(int(descifrado[i:i+8], 2))
-    
-print("texto:", texto)
+
+print("Texto:", texto)
 print("Llave:", llave)
-print("Cifrado:", cifrado)
+print("Cifrado:", texto_cifrado)
 print("Descifrado:", resultado)
